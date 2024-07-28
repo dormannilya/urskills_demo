b2d781cf1ae5be61933c2f4fb4516546267af08b
Eliyahu Dorman <93156122+dormannilya@users.noreply.github.com>
2024-07-28 21:48:40 +0300

    Updated project with new description toggle functionality

--- src/UrSkillsDemo.js
+++ src/UrSkillsDemo.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import './App.css';
+import './UrSkillsDemo.css';
+import urSkillsLogo from './assets/urskillslogo2.png';
 
-const App = () => {
+const UrSkillsDemo = () => {
   const [showDescription, setShowDescription] = useState(false);
 
   const handleToggleDescription = () => {
@@ -9,34 +10,32 @@ const App = () => {
   };
 
   return (
-    <div className="app">
-      <header className="app-header">
-        <img src="./src/assets/urskillslogo2.png" alt="UrSkills Logo" className="logo" />
-        <div className="button-container">
-          <a href="https://t.me/UrSkillsBot/UrSkillsApp" className="button_demo">Демонстрация UrSkills</a>
-          <a href="https://t.me/elielieli_d" className="button_connect">Связаться</a>
+    <div className="container">
+      <img src={urSkillsLogo} alt="UrSkills Logo" className="logo" />
+      <div className="button-container">
+        <a href="https://t.me/UrSkills/4" className="button_demo">Видео-демонстрация</a>
+        <a href="https://t.me/elielieli_d" className="button_connect">Связаться</a>
+      </div>
+      <div className="description-toggle">
+        <button className="toggle-button" onClick={handleToggleDescription}>
+          Показать описание
+        </button>
+      </div>
+      {showDescription && (
+        <div className="description">
+          <strong>UrSkills App</strong> — маркетплейс курсов и образовательная платформа на блокчейне TON в Telegram.
+          <br />
+          Децентрализованые оплаты обучающий курсов через TON: Автор продает продукты - Покупатель покупает продукты - Платформа получает комиссию.
+          <br />
+          Монетизация обучения: Ученики получают кэшбэк в TON за прохождения курсов. Учись и зарабатывай!
+          <br />
+          Новаторский маркетинг: Продавцам курсов больше не надо придумывать десятки сложных воронок продаж с большим количеством этапов на разных платформах
+          <br />
+          Текущая версия MVP: 0.9.0-alpha (релиз 27.07.2024)
         </div>
-        <div className="description-toggle">
-          <button className="toggle-button" onClick={handleToggleDescription}>
-            Показать описание
-          </button>
-        </div>
-        {showDescription && (
-          <div className="description">
-            <strong>UrSkills App</strong> — маркетплейс курсов и образовательная платформа на блокчейне TON в Telegram.
-            <br />
-            Децентрализованые оплаты обучающий курсов через TON: Автор продает продукты - Покупатель покупает продукты - Платформа получает комиссию.
-            <br />
-            Монетизация обучения: Ученики получают кэшбэк в TON за прохождения курсов. Учись и зарабатывай!
-            <br />
-            Новаторский маркетинг: Продавцам курсов больше не надо придумывать десятки сложных воронок продаж с большим количеством этапов на разных платформах
-            <br />
-            Текущая версия MVP: 0.9.0-alpha (релиз 27.07.2024)
-          </div>
-        )}
-      </header>
+      )}
     </div>
   );
 };
 
-export default App;
+export default UrSkillsDemo;
